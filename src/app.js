a9d790e52f588cac18d7ab7dc156d6fa281f0154
Bairon00 <bairon97@icloud.com>
2022-07-22 20:02:25 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,20 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+var ext = [".com", ".net", ".us", ".io"];
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let w = 0; w < noun.length; w++) {
+        for (let x = 0; x < ext.length; x++) {
+          console.log(pronoun[i] + adj[j] + noun[w] + ext[x]);
+        }
+      }
+    }
+  }
 };
